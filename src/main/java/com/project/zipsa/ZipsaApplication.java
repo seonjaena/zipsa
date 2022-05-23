fd4c48457a6e7fe3962bfa36b27cbe8daa992e64
나선재 <sky114z21@gmail.com>
2022-05-23 21:22:14 +0900

    Entity 기능 추가
    * ISSUE ID : ZIPSA-5
    
    Entity Auditing 자동 주입 기능 추가

--- src/main/java/com/project/zipsa/ZipsaApplication.java
+++ src/main/java/com/project/zipsa/ZipsaApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Optional;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ZipsaApplication {
 
