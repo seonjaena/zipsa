17e6c8fb0861691cc756e734c59796ac2bd8e574
나선재 <sky114z21@gmail.com>
2022-06-12 23:40:19 +0900

    Health Check 로깅 추가

--- src/main/java/com/project/zipsa/controller/HealthCheckController.java
+++ src/main/java/com/project/zipsa/controller/HealthCheckController.java
@@ -1,15 +1,18 @@
 package com.project.zipsa.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/healthcheck")
 public class HealthCheckController {
 
     @GetMapping
     public String get() {
+        log.info("Health Check Success");
         return "OK";
     }
 
