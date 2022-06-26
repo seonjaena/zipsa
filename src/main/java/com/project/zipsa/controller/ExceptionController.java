48f6308abd3d071cc4c561617cb86d77972f405a
나선재 <sky114z21@gmail.com>
2022-06-26 19:33:53 +0900

    수정

--- src/main/java/com/project/zipsa/controller/ExceptionController.java
+++ src/main/java/com/project/zipsa/controller/ExceptionController.java
@@ -2,24 +2,31 @@ package com.project.zipsa.controller;
 
 import com.project.zipsa.exception.custom.UnAuthenticatedException;
 import com.project.zipsa.exception.custom.UnAuthorizedException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @Slf4j
 @RestController
 @RequestMapping(value = "/exception")
+@RequiredArgsConstructor
 public class ExceptionController {
+
+    private final MessageSource messageSource;
 
     @GetMapping(value = "/entryPoint")
     public void entryPointException() {
-        throw new UnAuthenticatedException("인증에 실패했습니다.");
+        throw new UnAuthenticatedException(messageSource.getMessage("error.user.authentication", null, Locale.KOREA));
     }
 
     @GetMapping(value = "/accessDenied")
     public void accessDeniedException() {
-        throw new UnAuthorizedException("현재 권한으로 접근할 수 없습니다.");
+        throw new UnAuthorizedException(messageSource.getMessage("error.user.authority", null, Locale.KOREA));
     }
 
 }
--- src/main/java/com/project/zipsa/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/zipsa/security/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.project.zipsa.security;
 
+import com.project.zipsa.exception.custom.UnAuthenticatedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
