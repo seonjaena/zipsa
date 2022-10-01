b9d984bd1cf04535519d5afe3f09ef3210edf1c8
나선재 <sky114z21@gmail.com>
2022-10-01 20:07:13 +0900

    file 인식하게 수정

--- src/main/java/com/project/zipsa/config/FirebaseConfig.java
+++ src/main/java/com/project/zipsa/config/FirebaseConfig.java
@@ -4,9 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -14,8 +15,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void init(){
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/zipsa-firebase.json");
+            InputStream serviceAccount = new ClassPathResource("zipsa-firebase.json").getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
