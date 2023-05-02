dafd3f7e833253191ce324a10660ba5a278d08b8
나선재 <sky114z21@gmail.com>
2023-05-03 01:27:10 +0900

    QueryDSL 추가

--- src/test/java/com/project/zipsa/ZipsaApplicationTests.java
+++ src/test/java/com/project/zipsa/ZipsaApplicationTests.java
@@ -1,10 +1,16 @@
 package com.project.zipsa;
 
 import com.project.zipsa.controller.HealthCheckController;
+import com.project.zipsa.entity.QUsers;
+import com.project.zipsa.entity.Users;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -15,9 +21,21 @@ class ZipsaApplicationTests {
     @Autowired
     private HealthCheckController healthCheckController;
 
+    @Autowired
+    private JPAQueryFactory query;
+
     @Test
     void contextLoads() {
         assertThat(healthCheckController).isNotNull();
+    }
+
+    @Test
+    void test() {
+        List<Users> results = query
+                .selectFrom(QUsers.users)
+                .fetch();
+
+        Assertions.assertThat(results.size()).isSameAs(10);
     }
 
 }
--- src/test/java/com/project/zipsa/integration/Study.java
+++ src/test/java/com/project/zipsa/integration/Study.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
